451ff2df8944367f3a0336973839ece544b1afe2
Milan Chymčák <chymmi5@gmail.com>
2022-09-21 16:28:30 +0200

    textarea

--- build/blocks/innerTextarea/index.asset.php
+++ build/blocks/innerTextarea/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '88d2eaae0bd7c385cc78');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'b57bf6685792c4431ae2');
